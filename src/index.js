11205e356357f74cb9efd607fd238c069ea28180
laxmis-prog <laxmisanuu5@gmail.com>
2025-08-31 20:24:12 +0300

    Fix module not found error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { createRoot } from "react-dom/client"; {/*Connects your react app to the real DOM(html page) */}
-import App from "./App";
+import App from "./App.jsx";
+import ReactDom from "react-dom";
 
-const root = createRoot(document.getElementbyId("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <App />  {/*This renders the component, tells react to render App inside <div id="root"> */}
