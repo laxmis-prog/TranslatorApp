f37af41e540eeeec3ab07aa17ed24a137ed19e29
laxmis-prog <laxmisanuu5@gmail.com>
2025-09-02 09:25:17 +0300

    Add input with live state update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
+
 
 export default function App () {
+
+  const [text, setText] = useState("");
+  const [submittedText, setSubmittedText] = useState("");
+
+
    return (
     <div>
         <h1>Translator App</h1>
         <p>Created by Laxmi</p>
+
+        <input value={text} onChange={(event) => setText(event.target.value)} />
+        <p>You typed: {text}</p>
+
+       <button>
+        Translate
+       </button>
+
+
+
     </div>
    );
 
